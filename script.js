35ef769fecd714267ae764c6cfa15ccc6a569d04
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-30 20:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+const student = {
+  name: 'mister',
+};
+
+// Add getKeys method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
